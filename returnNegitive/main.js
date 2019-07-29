f9c70d19de5684586d17d2525cc45367db196d51
Resilient Coders 3 <resilientcoders3@Resilients-MacBook-Pro-2.local>
2019-07-28 21:41:10 -0400

    code

---
+++ returnNegitive/main.js
@@ -0,0 +1,14 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+//
+
+
+function makeNegative(num) {
+	if (num>0)	{
+  		return num * -1
+	}
+	else {
+		return num
+	}
+}
+
+makeNegative(-5)
---
+++ sentenceSmasher/main.js
@@ -0,0 +1,10 @@
+// Write a method smash that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Smash Words
+function smash (words) {
+    "use strict";
+    let thisthing = words.join(" ")
+    return thisthing
+
+
+};
