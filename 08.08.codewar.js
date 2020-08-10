511e5c6d05dd4dca1d6764a629a510479723ddba
Stacy <47963818+StacyLafume@users.noreply.github.com>
2020-08-09 22:10:35 -0700

    Update 08.08.codewar.js

--- 08.08.codewar.js
+++ 08.08.codewar.js
@@ -1,3 +1,22 @@
+// We'll create a function that takes in two parameters:
+// a sequence (length and types of items are irrelevant)
+// a function (value, index) that will be called on members of the sequence and their index. The function will return either true or false.
+// Your function will iterate through the members of the sequence in order until the provided function returns true; at which point your function will return that item's index.
+// If the function given returns false for all members of the sequence, your function should return -1.
+
+var findInArray = function(array, iterator) {
+  var result = -1
+  for (var i = 0; i < array.length; i++) {
+    if (iterator(array[i], i)) {
+      result = i
+      break
+    } else {
+      result = -1
+    }
+  }
+  return result
+};
+
 //Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 //The output should be two capital letters with a dot seperating them.
 //It should look like this:
