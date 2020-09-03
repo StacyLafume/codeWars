21503e06c4c0a218228599c14305991c545ec8f0
StacyLafume <stacylafume123@gmail.com>
2020-09-02 23:52:12 -0700

    evenIndex

---
+++ evenIndex.js
@@ -0,0 +1,18 @@
+// Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+
+// For example:
+
+// "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+// "a"             --> "invalid string"
+
+const evenChars = string => {
+    if(string.length < 2 || string.length > 100) return "invalid string";
+    const strArr = string.split('');
+    let evenIndex = [];
+    for(let i = 0; i<strArr.length; i++){
+      if( i % 2 !== 0){
+        evenIndex.push(strArr[i])
+      }
+    }
+    return evenIndex;
+  }
