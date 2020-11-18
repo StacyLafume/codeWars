a0330da6c9173b742bb39b2927e358424898f2cb
StacyLafume <stacylafume123@gmail.com>
2020-11-17 23:00:05 -0500

    tooslow.js

---
+++ tooslow.js
@@ -0,0 +1,9 @@
+// Make me slow! Calling makeMeSlow() should take at least 7 seconds.
+
+//https://www.codewars.com/kata/57f59da8d3978bb31f000152/train/javascript
+
+function makeMeSlow() {
+    // this function is too fast!
+    for (var x = 0; x < 7000000000; x++) {
+    }
+  }
